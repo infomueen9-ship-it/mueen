3d6a918249a66b37a67d78f00adec63438cf6d8a
Mueen <info@mueen.sa>
2026-05-09 10:43:10 +0300

    Update API URL to Railway

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '../store/authStore'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://mueen-production-fcb0.up.railway.app',
 })
 
 api.interceptors.request.use((config) => {
