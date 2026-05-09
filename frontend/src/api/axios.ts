29347536b16123020d1c1e7468a15b7e357277c2
Mueen <info@mueen.sa>
2026-05-09 10:37:17 +0300

    connect frontend to railway backend

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,14 +2,16 @@ import axios from 'axios'
 import { useAuthStore } from '../store/authStore'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8082',
+  baseURL: import.meta.env.VITE_API_URL,
 })
 
 api.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
+
   return config
 })
 
@@ -20,6 +22,7 @@ api.interceptors.response.use(
       useAuthStore.getState().logout()
       window.location.href = '/login'
     }
+
     return Promise.reject(error)
   }
 )
