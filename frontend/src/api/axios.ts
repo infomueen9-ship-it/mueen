c20821ea620d1bd8656a14effb5a81946acc1d1f
mona ali <infomueen9@gmail.com>
2026-07-31 08:52:34 +0300

    Update logo image

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '../store/authStore'
 
 const api = axios.create({
-  baseURL: 'http://54.83.121.129:8082',
+ baseURL: 'http://54.83.121.129/api',
 })
 
 api.interceptors.request.use((config) => {
