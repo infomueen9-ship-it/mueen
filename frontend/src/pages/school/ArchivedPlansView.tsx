fc25f743882caf9e38694bc77869aead58bc11f7
mona ali <infomueen9@gmail.com>
2026-08-16 10:24:23 +0300

    Fix study plans data mapping

--- frontend/src/pages/school/ArchivedPlansView.tsx
+++ frontend/src/pages/school/ArchivedPlansView.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ArrowRight, Plus, Save, Trash2 } from 'lucide-react'
 import api from '../../api/axios'
 import toast from 'react-hot-toast'
